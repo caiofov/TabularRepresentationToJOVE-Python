6fe92cdbe7e6bad07d019c595d86d7059da0a94f
caiofov <cfoviana@alu.ufc.br>
2022-11-21 19:30:37 -0300

    main function

--- conversor/main.py
+++ conversor/main.py
@@ -17,17 +17,25 @@ def parse_state(state):
     return state
 
 
-with open("input.csv") as f:
-    reader = csv.reader(f)
-    l = list(reader)
-    header = l[0][1:]
-    states = l[1:]
-    del l
+def main() -> None:
+    with open("input.csv") as f:
+        reader = csv.reader(f)
+        l = list(reader)
+        header = l[0][1:]
+        states = l[1:]
+        del l
 
-for state in states:
-    st = parse_state(state[0])
-    transitions = state[1:]
-    for next_st, letter in zip(transitions, header):
-        next_st = parse_state(next_st)
-        output += f"{st}: {letter} -> {next_st}\n"
-print(output)
+    for state in states:
+        st = parse_state(state[0])
+        transitions = state[1:]
+        for next_st, letter in zip(transitions, header):
+            next_st = parse_state(next_st)
+            output += f"{st}: {letter} -> {next_st}\n"
+    with open("output.txt", "w") as out:
+        out.write(output)
+
+    print(output)
+
+
+if __name__ == "__main__":
+    main()
